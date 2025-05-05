efe4d2bbd3bf3b1aa6261e21f25165ae12dc3995
Eitan Borgnia <eborgnia2@gmail.com>
2025-05-06 00:18:48 +0100

    debug

--- src/llmperf/ranker_benchmark.py
+++ src/llmperf/ranker_benchmark.py
@@ -34,8 +34,8 @@ def get_token_throughput_latencies(
         time.monotonic() - start_time < test_timeout_s
         and len(completed_requests) < max_num_completed_requests
     ):
-        query = rows[0]["query"]
-        codebase = rows[0]["codebase"]
+        query = rows[2]["query"]
+        codebase = rows[2]["codebase"]
         ranker_config = RankerConfig(
             query=query,
             codebase=codebase,
