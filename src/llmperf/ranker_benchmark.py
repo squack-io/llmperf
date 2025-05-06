3dcb284d8c9d834a884b5e9cd7466d2a0f8943cf
Eitan Borgnia <eborgnia2@gmail.com>
2025-05-06 01:21:43 +0100

    changed the token count

--- src/llmperf/ranker_benchmark.py
+++ src/llmperf/ranker_benchmark.py
@@ -34,8 +34,8 @@ def get_token_throughput_latencies(
         time.monotonic() - start_time < test_timeout_s
         and len(completed_requests) < max_num_completed_requests
     ):
-        query = rows[5]["query"]
-        codebase = rows[5]["codebase"]
+        query = rows[6]["query"]
+        codebase = rows[6]["codebase"]
         ranker_config = RankerConfig(
             query=query,
             codebase=codebase,
